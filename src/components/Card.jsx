ac89cde955c6b388875ca3d06605693b1c9c8440
Saleem Rawn <29518880+saleemrawn@users.noreply.github.com>
2025-11-26 19:27:26 +0000

    fix: update class attr to className

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ export default function Card({
 }) {
   return (
     <button className="card" onClick={onClick} id={id} style={{ backgroundImage: `url(${imgUrl})` }}>
-      <span class="sr-only">{photoAlt}</span>
+      <span className="sr-only">{photoAlt}</span>
       <span className="card-name">
         Photo by <a href={photographerUrl}>{photographer}</a> on <a href={websiteUrl}>{websiteName}</a>
       </span>
